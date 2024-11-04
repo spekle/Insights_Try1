bad4e454f175fb5e107286177d29a3610f2cb720
Spekle Solutions <developer@speklesolutions.com>
2024-11-04 14:23:19 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from fastapi import FastAPI
+!pip install pandas
+import pandas as pd
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
