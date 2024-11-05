5276338df902b3663b0bda271fd555762777252f
Spekle Solutions <developer@speklesolutions.com>
2024-11-04 19:15:02 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi import Query
+
+app = FastAPI()
+
+@app.get("/")
+async def root(input_text: str = Query(None, description="Input text from the user")):
+    return {"echo": input_text}
