b31099fc0e5bddebe4b11c073c1b6914d4d534cf
Spekle Solutions <developer@speklesolutions.com>
2024-11-04 19:14:46 -0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from fastapi import Query
-
-app = FastAPI()
-
-@app.get("/")
-async def root(input_text: str = Query(..., description="Input text from the user")):
-    return {"echo": input_text}
